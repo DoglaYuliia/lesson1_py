b843df4e8d380aab4c4f065005c22df7203cf622
My Name <dogla3@gmail.com>
2023-09-03 11:30:29 +0200

    add

--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-print('Hello, World!')
+# print('Hello, World!')
+# print('Hello, World!')
+# print('Hello, World!')
+# print('Hello, World!')
+#
+# print(121, 'QWE', '1234')
+#
+# print("1" + "2" + "3")
+# print("1" + "2" * 3)
+# print(("1" + "2") * 3)
+# print(20 // 2)
+# print(20 / 2)
+# print(21 // 2)
+# print(-21 // 2)
+# print(-21 / 2)
+
+##########
+
+a = 'Hello, "A"'
+b = a
+c = 'Hello, "B"'
+b = c
+c = a
+a = b
+print(a)
+
+#############
+
+my_value_1 = 2
+my_value_2 = '4'
+print(my_value_1 * my_value_2)
+
+##############
+
+my_value_1 = 2
+my_value_2 = '4'
+my_value_1 = str(my_value_1)
+my_value_2 = int(my_value_2)
+print(my_value_1 * my_value_2)
+
+##############
+
+my_value_1 = 2
+my_value_2 = '4'
+my_value_2 = int(my_value_2)
+print(my_value_1 * my_value_2)
+
+################
+
+my_value_1 = 2
+my_value_2 = '4'
+my_value_1 = str(my_value_1)
+print(my_value_1 + my_value_2)
+
+################
+
+my_value = 12
+my_bool = my_value % 3 == 0
+print(my_bool)
+
+################
+
+my_value = 12
+my_bool_1 = my_value % 3 == 0
+my_bool_2 = my_value % 2 == 0
+print(my_bool_1 and my_bool_2)
+
+################
+
+my_value = 14
+my_bool_1 = my_value % 3 == 0
+my_bool_2 = my_value % 2 == 0
+print(my_bool_1 or my_bool_2)
+
+#############
+
+my_value = 7
+my_bool_1 = my_value % 3 == 0
+my_bool_2 = my_value % 2 == 0
+print(not my_bool_1 and not my_bool_2)
+
+##############
